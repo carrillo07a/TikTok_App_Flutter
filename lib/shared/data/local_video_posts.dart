b389a30a13872cd0566df525178f6316aaf94177
Aurelio Carrillo <carrillo07a@gmail.com>
2023-05-14 02:18:56 -0600

    TikTok App Flutter

---
+++ lib/shared/data/local_video_posts.dart
@@ -0,0 +1,92 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Subiendo escaleras automáticas',
+    'videoUrl': 'assets/videos/1.mp4',
+    'likes': 23230,
+    'views': 1523,
+  },
+  {
+    'name': 'Planta apreciada por peatones',
+    'videoUrl': 'assets/videos/2.mp4',
+    'likes': 24230,
+    'views': 1343,
+  },
+  {
+    'name': 'Que borroso veo todo!',
+    'videoUrl': 'assets/videos/3.mp4',
+    'likes': 21564320,
+    'views': 123563,
+  },
+  {
+    'name': '¿Esto es trigo? que interesante',
+    'videoUrl': 'assets/videos/4.mp4',
+    'likes': 320,
+    'views': 2300,
+  },
+  {
+    'name': 'El COVID no me afecta',
+    'videoUrl': 'assets/videos/5.mp4',
+    'likes': 3230,
+    'views': 31030,
+  },
+  {
+    'name': 'No quiero ir a trabajar hoy señor Stark',
+    'videoUrl': 'assets/videos/6.mp4',
+    'likes': 10,
+    'views': 330,
+  },
+  {
+    'name': 'Limpiar nunca fue tan divertido',
+    'videoUrl': 'assets/videos/7.mp4',
+    'likes': 1320,
+    'views': 33032,
+  },
+  {
+    'name': '¿Ya llegamos a la India?... umm si',
+    'videoUrl': 'assets/videos/8.mp4',
+    'likes': 342,
+    'views': 3332,
+  },
+  {
+    'name': 'Conduciendo en la ciudad!!',
+    'videoUrl': 'assets/videos/9.mp4',
+    'likes': 142,
+    'views': 1232,
+  },
+  {
+    'name': 'Musica..',
+    'videoUrl': 'assets/videos/10.mp4',
+    'likes': 477,
+    'views': 3882,
+  },
+  {
+    'name': 'Tomando Frappe!!',
+    'videoUrl': 'assets/videos/11.mp4',
+    'likes': 692,
+    'views': 5332,
+  },
+  {
+    'name': 'Estudio',
+    'videoUrl': 'assets/videos/12.mp4',
+    'likes': 312,
+    'views': 2239,
+  },
+  {
+    'name': 'Juganfo jenga!!',
+    'videoUrl': 'assets/videos/13.mp4',
+    'likes': 542,
+    'views': 1742,
+  },
+  {
+    'name': 'Torre!!',
+    'videoUrl': 'assets/videos/14.mp4',
+    'likes': 102,
+    'views': 742,
+  },
+  {
+    'name': 'Motos!!',
+    'videoUrl': 'assets/videos/15.mp4',
+    'likes': 32,
+    'views': 650,
+  },
+];
